73826cacb510e617d4c2491bf69289eb31789426
girijeshcse <girijeshcse@users.noreply.github.com>
2021-09-06 01:11:50 +0000

    deploy: b0b00f478f78e0172634f3b98f998e82ccb8b248

--- assets/js/search.js
+++ assets/js/search.js
@@ -47,7 +47,7 @@ jtd.onReady = function(ready) {
 
 function initSearch() {
     var request = new XMLHttpRequest();
-    request.open('GET', '/scribble-code/assets/js/search-data.json', true);
+    request.open('GET', '/scribble/assets/js/search-data.json', true);
   
     request.onload = function(){
       if (request.status >= 200 && request.status < 400) {
